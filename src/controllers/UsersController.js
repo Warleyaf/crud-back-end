05e479e932986946d08014da8d6931a8e1329b6f
warley <warley@pop-os.localdomain>
2022-06-24 20:52:29 -0300

    Adicionando o controlador de usuário que vai ser o CRUD, que vai ter a listagem, visualização de 1, Atualização, e remoção, adicionei também as rotas do CRUD

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,54 @@
+// COntrollers de usuários e o crud de usuários também que vai ter, Listagem, a visualização de 1, Atualização, e a remoção.
+
+import User from '../models/User';
+
+class UsersController {
+   //Listagem
+   async index(req, res) {
+      // try catch é meio que um if do erro, caso ele tente fazer alguma coisa e der uma erro vai me retornar o erro ou seja o catch
+      try {
+         // o método find() sem alguma parâmetro entre parenteses, vai listar todos os usuários daquela coleção
+         const users = await User.find();
+         return res.json(users);
+      } catch(err) {
+         console.error(err);
+         return res.sttus(500).json({error: "Internal server error."})
+      }
+   }
+
+   async show(req, res) {
+
+   }
+
+   async create(req, res) {
+      try {
+         const { email, password } = req.body;
+
+         const user = await User.findOne({email});
+         // nesse if é para verificar se já tem um e-mail cadastro igual, caso tenha vai me retorna o erro
+         if(user) {
+            return res
+            .status(422)
+            .json({message: `User ${email} already exists.`})
+         }
+
+         const newUser = await User.create({email, password})
+
+         return res.status(201).json(newUser)
+
+      } catch(err) {
+         console.error(err);
+         return res.sttus(500).json({error: "Internal server error."})
+      }
+   }
+
+   async update(req, res) {
+
+   }
+
+   async destroy(req, res) {
+
+   }
+}
+
+export default new UsersController();
