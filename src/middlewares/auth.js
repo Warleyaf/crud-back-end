9e5b55e455a3a07c0c4725f35187819787cc78ae
warley <warley@pop-os.localdomain>
2022-06-27 21:19:38 -0300

    Adicionando a bilioteca JWT, e adicionando a lógica de autorização do JWT do token

---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+import promisify from 'util';
+
+import authConfig from '../config/auth';
+
+export default async (req, res, next) => {
+   const authHeader = req.headers.authorization;
+   //Lógica da autorização do token
+
+   if(!authHeader) {
+      return res.status(401).json({error: 'Token was not provided.'})
+   }
+
+   //pegando o token caso passe
+
+   const [, token] = authHeader.split(' ');
+
+   try {
+      const decoded = await promisify(jwt.verify(token, authConfig.secret));
+      req.userId = decoded.id;
+
+      return next();
+   } catch (err) {
+      return res.status(401).json({error: 'Invalid token.'})
+   }
+}
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,20 @@ import HelloController from "./controllers/HelloController";
 import UsersController from "./controllers/UsersController";
 
 import RepositoriesController from "./controllers/RepositoriesController";
+import auth from "./middlewares/auth";
 
 const routes = new Router();
 
+
+// ------ tudo que estiver daqui pra baixo meio que vai estar protegido pelo nosso JWT
+// para isso preciso adicionar a biblioteca jsonwebtoken, usando o comando yarn add jsonwebtoken
+
 routes.get('/hello', HelloController.index);
 
+// routes.use(auth) controle do middleware, daqui pra cima publico, e daqui pra baixo privado
+routes.use(auth)
 
+// Rotas privadas
 // Formato de criar rota e essa é a Restfull
 routes.get('/users', UsersController.index);
 routes.get('/users/:id', UsersController.show);
